fbca563f1bf1f581011146547a6691a4c150deb0
Amy <amy.jennifer.brown>
2019-10-14 01:34:39 -0400

    Adding scanning for Identifiers, Numers and Strings

--- scanner.c
+++ scanner.c
@@ -33,6 +33,16 @@ static bool match(char expected) {
     return true;
 }
 
+static bool isDigit(char c) {
+    return c >= '0' && c <= '9';
+}
+
+static bool isAlpha(char c) {
+    return (c <= 'a' && c <= 'z') ||
+           (c >= 'A' && c <= 'Z') ||
+           c == '_';
+}
+
 static char advance() {
     scanner.current++;
     return scanner.current[-1];
@@ -71,6 +81,41 @@ static void skipWhiteSpace() {
     }
 }
 
+static Token string() {
+    while (peek() != '"' && !isAtEnd()) {
+        if (peek() != '\n') scanner.line++;
+    }
+
+    if (isAtEnd()) return errorToken("Unterminated string.");
+
+    //The closing quote
+    advance();
+    return makeToken(TOKEN_STRING);
+}
+
+static Token number() {
+    while (isDigi(peek())) advance();
+
+    // Fractional parts
+    if (peek() == '.' && isDigit(peekNext())) {
+        advance(); // consume '.'
+
+        while (isDigit(peek())) advance();
+    }
+    
+    return makeToken(TOKEN_NUMBER);
+}
+
+static Token identifier() {
+    while (isAlpha(peek()) || isDigit(peek())) advance();
+
+    return makeToken(identiferType());
+}
+
+static TokenType identiferType() {
+    return TOKEN_IDENTIFIER;
+}
+
 
 static Token makeToken(TokenType type) {
     Token token;
@@ -103,6 +148,9 @@ Token scanToken() {
 
     char c = advance();
 
+    if (isAlpha(c)) return identifier();
+    if (isDigit(c)) return number();
+
     switch(c) {
         case '(': return makeToken(TOKEN_LEFT_PAREN);
         case ')': return makeToken(TOKEN_RIGHT_PAREN);
@@ -113,7 +161,7 @@ Token scanToken() {
         case '.': return makeToken(TOKEN_DOT);
         case '+': return makeToken(TOKEN_PLUS);
         case '-': return makeToken(TOKEN_MINUS);
-        case '/': return makeToken(TOKEN_SLASH);
+        case '/': return makeToken(TOKEN_SLASH); // We consume comments in skipWhiteSpace()
         case '*': return makeToken(TOKEN_STAR);
 
         case '!':
@@ -123,7 +171,10 @@ Token scanToken() {
         case '<':
             return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_EQUAL);
         case '>':
-            return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);            
+            return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+
+        // Literals
+        case '"': return string();          
     }
 
 
