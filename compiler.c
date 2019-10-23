49246c1c891cc9fd95443b7d2e64d8c4ae60aedd
Amy <amy.jennifer.brown>
2019-10-22 20:08:26 -0400

    Added frontend and backend to compiler

--- compiler.c
+++ compiler.c
@@ -9,24 +9,100 @@
 #include "console.h"
 
 
-void compile(const char* source){
-    // Currently scanns through and outputs token 
-    initScanner(source);
-    int line = -1;
-    
+typedef struct {
+    Token current;
+    Token previous;
+    bool hadError;
+    bool panicMode;
+} Parser;
+
+
+Parser parser;
+
+
+Chunk* compilingChunk;
+
+static Chunk* currentChunk() {
+    return compilingChunk;
+}
+
+static void errorAt(Token* token, const char* message) {
+    if (parser.panicMode) return;
+
+    parser.panicMode = true;
+
+    fprintf(stderr, "%s[line %d] Error", RED, token->line);
+
+    if (token->type == TOKEN_EOF) {
+        fprintf(stderr, " at end");
+    } else if (token->type == TOKEN_ERROR) {
+        // do nothing
+    } else {
+        fprintf(stderr, "at '%.*s'",token->length, token->start);
+    }
+    fprintf(stderr,  ": %s%s\n", message, RESET_COLOR);
+    parser.hadError = true;
+}
+
+static void error(const char* message) {
+    errorAt(&parser.previous, message);
+}
+
+static void errorAtCurrent(const char* message) {
+    errorAt(&parser.current, message);
+}
+
+static void advance() {
+    parser.previous = parser.current;
+
     for (;;) {
-        //debugPrint("Compiling token...");
+        parser.current = scanToken();
+        if (parser.current.type != TOKEN_ERROR) break;
 
-        Token token = scanToken();
-
-        if (token.line != line) {
-            printf("%4d ", token.line);
-            line = token.line;
-        } else {
-            printf("   | ");
-        }
-        printf("%2d '%.*s'\n", token.type, token.length, token.start);
-
-        if (token.type == TOKEN_EOF) break;
+        errorAtCurrent(parser.current.start);
     }
 }
+
+static void consume(TokenType type, const char* message) {
+    if (parser.current.type == type) {
+        advance();
+        return;
+    }
+}
+
+static void emitByte(uint8_t byte) {
+    writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+    emitByte(byte1);
+    emitByte(byte2);
+}
+
+static void emitReturn() {
+    emitByte(OP_RETURN);
+}
+
+static void endCompiler() {
+    emitReturn();
+}
+
+
+bool compile(const char* source, Chunk* chunk){
+    // Currently scanns through and outputs token 
+    initScanner(source);
+
+    compilingChunk = chunk;
+
+    parser.hadError = false;
+    parser.panicMode = false;
+
+    int line = -1;
+    advance();
+    expression();
+    consume(TOKEN_EOF, "Expect end of expression.");
+    endCompiler();
+
+    return !parser.hadError;
+}
+
